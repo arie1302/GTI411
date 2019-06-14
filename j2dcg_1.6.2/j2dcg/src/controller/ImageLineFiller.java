6eb9bb1d62b0d54aec3ba16186428f907b7bd45c
Unknown <beaudry.ariane1302@gmail.com>
2019-06-14 14:48:49 -0400

    Remplissage Fonctionnelle

--- j2dcg_1.6.2/j2dcg/src/controller/ImageLineFiller.java
+++ j2dcg_1.6.2/j2dcg/src/controller/ImageLineFiller.java
@@ -25,8 +25,8 @@ import java.util.Stack;
 /**
  * <p>Title: ImageLineFiller</p>
  * <p>Description: Image transformer that inverts the row color</p>
- * <p>Copyright: Copyright (c) 2003 Colin Barré-Brisebois, Éric Paquette</p>
- * <p>Company: ETS - École de Technologie Supérieure</p>
+ * <p>Copyright: Copyright (c) 2003 Colin Barrï¿½-Brisebois, ï¿½ric Paquette</p>
+ * <p>Company: ETS - ï¿½cole de Technologie Supï¿½rieure</p>
  * @author unascribed
  * @version $Revision: 1.12 $
  */
@@ -91,35 +91,71 @@ public class ImageLineFiller extends AbstractTransformer {
 	}
 
 	private void borderFill(Point ptClicked) {
+		boolean borderbool = false;
+		Point BorderStarter ;
 		Stack stack = new Stack();
 		stack.push(ptClicked);
 		Pixel baseLinePixel  = currentImage.getPixel(ptClicked.x, ptClicked.y);
 
-		while (!stack.empty()) {
+		//Recherche de bordure
+		while (!stack.empty() && !borderbool) {
 			Point current = (Point)stack.pop();
 			if (0 <= current.x && current.x < currentImage.getImageWidth() &&
-				!currentImage.getPixel(current.x, current.y).equals(borderColor)) {
-
-				System.out.println("current pixel color: " + currentImage.getPixel(current.x, current.y));
-				//System.out.println("baseline: "+ baseLinePixel);
-				//System.out.println(currentImage.getPixel(current.x, current.y).equals(baseLinePixel));
+				!currentImage.getPixel(current.x, current.y).equals(fillColor)) {
 				
-				if(currentImage.getPixel(current.x, current.y).equals(baseLinePixel)) {	
-					//System.out.println("interieur image");
-						// Next points to fill.
-						Point nextLeft = new Point(current.x-1, current.y);
-						Point nextRight = new Point(current.x+1, current.y);	
-						
-						stack.push(nextLeft);
-						stack.push(nextRight);
-						
-				}else if(!currentImage.getPixel(current.x, current.y).equals(borderColor)){
+				if(currentImage.getPixel(current.x, current.y).equals(baseLinePixel)) {
 					
-					currentImage.setPixel(current.x, current.y, borderColor);					
+					// Next points to fill.
+					Point nextLeft = new Point(current.x-1, current.y);
+					Point nextRight = new Point(current.x+1, current.y);
+					Point nextDown = new Point(current.x, current.y-1);
+					Point nextUp = new Point(current.x, current.y+1);
 					
-					System.out.println("border line pixel : "+ currentImage.getPixel(current.x, current.y));
+					stack.push(nextLeft);
+					stack.push(nextRight);
+					stack.push(nextDown);
+					stack.push(nextUp);
+				}else {
+					borderbool = true;
+				    baseLinePixel  = currentImage.getPixel(current.x, current.y);
+				    System.out.println("BORDER!!!!!!   "+baseLinePixel);
+				    BorderStarter = new Point(current.x, current.y);
+				    stack.push(BorderStarter);
 				}
 
+			}
+		}
+		
+		//Remplissage de bordure 
+		while (!stack.empty()  && borderbool) {
+			Point current = (Point)stack.pop();
+			if (0 <= current.x && current.x < currentImage.getImageWidth()) {
+				System.out.println("Remplissage");
+				if (!currentImage.getPixel(current.x, current.y).equals(borderColor)) {
+
+					// System.out.println("current: " + currentImage.getPixel(current.x,
+					// current.y));
+					// System.out.println("baseline: "+ baseLinePixel);
+					// System.out.println(currentImage.getPixel(current.x,
+					// current.y).equals(baseLinePixel));
+
+					if (currentImage.getPixel(current.x, current.y).equals(baseLinePixel)) {
+
+						currentImage.setPixel(current.x, current.y, borderColor);
+
+						// Next points to fill.
+						Point nextLeft = new Point(current.x - 1, current.y);
+						Point nextRight = new Point(current.x + 1, current.y);
+						Point nextDown = new Point(current.x, current.y - 1);
+						Point nextUp = new Point(current.x, current.y + 1);
+
+						stack.push(nextLeft);
+						stack.push(nextRight);
+						stack.push(nextDown);
+						stack.push(nextUp);
+					}
+
+				} 
 			}
 		}
 		
